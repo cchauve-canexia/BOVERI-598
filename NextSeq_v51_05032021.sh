5087d92d6bfcc47ce91bfbfc3a8bec237688bcdc
cchauve-canexia <cchauve@canexiahealth.com>
2021-03-06 02:40:40 +0000

    report and indels files

--- NextSeq_v51_05032021.sh
+++ NextSeq_v51_05032021.sh
@@ -3,3 +3,4 @@
 echo "Extract results for NextSeq v51 samples"
 python bin/extract_files.py data/NextSeq_v51_05032021.csv
 python bin/dump_variants.py data/NextSeq_v51_05032021.csv -m CG001v5.1_Amplicon_Manifest_Panel5.1.12_20200911.tsv
+python bin/check_indels.py data/NextSeq_v51_05032021.csv data/NextSeq_blacklist_03032021.tsv
