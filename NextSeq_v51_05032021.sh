d35c801f62bd1d24185362ae1cd66c7469ba865b
cchauve-canexia <69211178+cchauve-canexia@users.noreply.github.com>
2021-03-08 09:39:13 -0800

    Delete NextSeq_v51_05032021.sh

--- NextSeq_v51_05032021.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Extract results for NextSeq v51 samples"
-python bin/extract_files.py data/NextSeq_v51_05032021.csv
-python bin/dump_variants.py data/NextSeq_v51_05032021.csv -m CG001v5.1_Amplicon_Manifest_Panel5.1.12_20200911.tsv
-python bin/check_indels.py data/NextSeq_v51_05032021.csv data/NextSeq_blacklist_03032021.tsv
